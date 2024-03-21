15d4212abd2051df31521cf00c689ca4630cd110
sushantn16 <nandwani160698@gmail.com>
2024-03-21 18:14:50 +0100

    fix

--- src/components/milestone.tsx
+++ src/components/milestone.tsx
@@ -7,10 +7,8 @@ export interface MilestoneProps {
     return (
       <div className="flex flex-row items-center gap-4">
         <div className="w-12 h-4 rounded-full border-2 border-gray-200 dark:border-gray-800 bg-gray-200 dark:bg-gray-800" />
-        <div className="grid gap-1">
           <h3 className="text-sm font-medium">{title}</h3>
           <p className="text-sm text-gray-500 dark:text-gray-400">{description}</p>
-        </div>
       </div>
     );
   };
