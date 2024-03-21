e11407c47d40fc1f8dc4d364b72e18c606a768d9
sushantn16 <nandwani160698@gmail.com>
2024-03-21 21:14:44 +0100

    style improvements

---
+++ src/app/investors/page.tsx
@@ -0,0 +1,7 @@
+export default function Investors() {
+  return (
+    <div>
+      <h1>Investors coming soon!</h1>
+    </div>
+  );
+}
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,38 +2,45 @@
 import Link from "next/link";
 import { usePathname } from 'next/navigation'
 import { Button } from "@/components/ui/button";
-import { Calendar, Package2Icon, PackageIcon } from "lucide-react";
+import { Calendar, ChevronUp, Package2, PackageIcon } from "lucide-react";
 
 export default function Sidebar() {
-  const pathname = usePathname()
-  return (
-    <div className="hidden border-r bg-gray-100/40 lg:block dark:bg-gray-800/40">
-      <div className="flex h-full max-h-screen flex-col gap-2">
-        <div className="flex h-[60px] items-center border-b px-6">
-          <Link className="flex items-center gap-2 font-semibold" href="#">
-            <Package2Icon className="h-6 w-6" />
-            <span className="">Acme Inc</span>
-          </Link>
+    const pathname = usePathname()
+    return (
+        <div className="hidden border-r bg-gray-100/40 lg:block dark:bg-gray-800/40">
+            <div className="flex h-full max-h-screen flex-col gap-2">
+                <div className="flex h-[60px] items-center border-b px-6">
+                    <Link className="flex items-center gap-2 font-semibold" href="#">
+                        <ChevronUp className="w-6 h-6" />
+                        <span className="">Start FELLOWSHIP</span>
+                    </Link>
+                </div>
+                <div className="flex-1 overflow-auto py-2">
+                    <nav className="grid items-start px-4 text-sm font-medium">
+                        <Link
+                            className={`flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 ${pathname === "/dashboard" ? "text-gray-900 dark:text-gray-50" : ""}`}
+                            href="/dashboard"
+                        >
+                            <PackageIcon className="h-4 w-4" />
+                            Startups
+                        </Link>
+                        <Link
+                            className={`flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 ${pathname === "/events" ? "text-gray-900 dark:text-gray-50" : ""}`}
+                            href="/events"
+                        >
+                            <Calendar className="h-4 w-4" />
+                            Events
+                        </Link>
+                        <Link
+                            className={`flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 ${pathname === "/events" ? "text-gray-900 dark:text-gray-50" : ""}`}
+                            href="/investors"
+                        >
+                            <Package2 className="h-4 w-4" />
+                            Investors
+                        </Link>
+                    </nav>
+                </div>
+            </div>
         </div>
-        <div className="flex-1 overflow-auto py-2">
-          <nav className="grid items-start px-4 text-sm font-medium">
-            <Link
-              className={`flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 ${pathname === "/dashboard" ? "text-gray-900 dark:text-gray-50" : ""}`}
-              href="/dashboard"
-            >
-              <PackageIcon className="h-4 w-4" />
-              Startups
-            </Link>
-            <Link
-              className={`flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50 ${pathname === "/events" ? "text-gray-900 dark:text-gray-50" : ""}`}
-              href="/events"
-            >
-              <Calendar className="h-4 w-4" />
-              Events
-            </Link>
-          </nav>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
