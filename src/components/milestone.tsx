ce2f5534983f2fc12b86df43aebce1e02776b370
0xsuid <0xsuid@protonmail.com>
2024-03-21 18:23:43 +0100

    Revert "fix width"
    This reverts commit 18a08bee63a51ba2c5de06e43e5205388e0e8408.

--- src/components/milestone.tsx
+++ src/components/milestone.tsx
@@ -6,7 +6,7 @@ export interface MilestoneProps {
   const Milestone: React.FC<MilestoneProps> = ({ title, description}) => {
     return (
       <div className="flex flex-row items-center gap-4">
-        <div className="w-12 h-4 rounded-full border-2 border-gray-200 dark:border-gray-800 bg-gray-200 dark:bg-gray-800" />
+        <div className="w-4 h-4 rounded-full border-2 border-gray-200 dark:border-gray-800 bg-gray-200 dark:bg-gray-800" />
         <div className="grid gap-1">
           <h3 className="text-sm font-medium">{title}</h3>
           <p className="text-sm text-gray-500 dark:text-gray-400">{description}</p>
